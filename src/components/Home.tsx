3f82a73bb04bc34b285890ee367ce61dcdd43776
NekoSushii <taypanghing@gmail.com>
2023-10-23 00:33:48 +0800

    1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -105,46 +105,6 @@ function Home(){
 return(
     <div>
       {content()}
-{/* 
-      <div className='blog_container'>
-        <div className='bodytext-left'>
-          <header className='title_header'>First Cycling Trip With StanChart Gang</header>
-          <h2>22 September 2023</h2>
-          <p>When Cycling with the StanChart Gang, had nice dinner together afterwords, thanks for the food dear!
-          </p>
-        </div>
-        <div className='imgcontainer-picture'>
-          <div className='animate__zoomInRight'>
-            <img src={imageThree} className='mainpageimg-right' alt=''/>
-          </div>
-        </div>
-      </div>
-
-      <div className='container reveal'>
-        <div className='blog_container'>
-          <div className='bodytext-right'>
-            <header className='title_header'>Food</header>
-            <h2>16 September 2023</h2>
-            <p>Dearest made food for me, taste good!</p>
-          </div>
-          <div className='imgcontainer-picture'>
-            <img src={imageTwo} className='mainpageimg-left' alt=''/>
-          </div>
-        </div>
-      </div>
-
-      <div className='container reveal'>
-        <div className='blog_container'>
-          <div className='bodytext-left'>
-            <header className='title_header'>Dearest Birthday</header>
-            <h2>15 September 2023</h2>
-            <p>Happy Birthday Dear!</p>
-          </div>
-          <div className='imgcontainer-picture'>
-            <img src={imageOne} className='mainpageimg-right' alt=''/>
-          </div>
-        </div>
-      </div> */}
 
     </div>
 )
